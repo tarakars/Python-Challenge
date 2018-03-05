c84cf8a7615f3aff63933db02645478f742e8f72
Taraka Rajeev Sharma Narayanabatla <tarakars_n@hotmail.com>
2018-03-05 15:18:06 -0500

    PyPoll

---
+++ PyPoll/main.py
@@ -0,0 +1,41 @@
+import csv
+import os
+import pandas as pd
+import numpy as py
+
+
+votes=[]
+percentage = []
+candidates=[]
+final_candidates=[]
+candidates_count=[]
+
+election_data_path = os.path.join("raw_data", "election_data_1.csv")
+
+print("Election Results")
+print("----------------------------")
+
+with open(election_data_path, newline="") as csvfile:
+    election_data = csv.reader(csvfile, delimiter = ",")
+
+    next(election_data,None)
+
+    for row in election_data:
+        candidates.append(row[2])
+        votes.append(row[0])
+    total_votes = len(votes)
+    print("Total Votes:" + str(total_votes))
+    print("----------------------------------")
+
+    for i in candidates:
+        if i not in final_candidates:
+            final_candidates.append(i)
+    for j in range(len(final_candidates)):
+            candidates_count.append(candidates.count(final_candidates[j]))
+            percentage.append((candidates_count[j]/total_votes)*100)
+            print(final_candidates[int(j)]+ " : " + str(candidates_count[int(j)]) + " ( " + str("{0:.0f}%".format(percentage[j])) + " ) ")     
+
+    winner = max(candidates)
+    print("----------------------------------")
+    print("Winner:" + winner)
+    print("----------------------------------")
---
+++ Pybank/main.py
@@ -0,0 +1,38 @@
+# first is first - import all the libraries we need to perform the tasks
+import csv
+import os
+import pandas as pd
+import numpy
+
+revenue=[] # declare and array to store revenue as a list from the csv file
+date=[] # declare and array to store date as a list from the csv file
+rev_change=[] # declare and array to store change in revenue as list
+
+print("Financial Analysis")
+print("------------------------------------------------------------------")
+
+
+budget_data_path = os.path.join("raw_data","budget_data_1.csv") # declaring the path for the CSV
+
+with open(budget_data_path,newline="") as csvfile: # making sure our operations are done while opening the CSV
+    budget_data = csv.reader(csvfile, delimiter = ",") # reading the csv file in to an object
+
+    next(budget_data,None) # skipping the first row to avoid header in math operations below
+
+    for row in budget_data: # creating a loop through the csv and create two arrays revenue and date
+        revenue.append(float(row[1]))
+        date.append(row[0])
+    print("Total number of months:" + str(len(date))) # len function is used to get the length of the array so we know how many months we have
+    print("Total Revenue:" + str(sum(revenue))) # sum function is used to get the sum of the array so we know the revenue
+
+    for i in range(1,len(revenue)): # creating a loop through the revenue array to calculate the average revenue change
+        rev_change.append(revenue[i] - revenue[i-1])
+        average_rev_change = sum(rev_change)/len(rev_change)
+    print("average revenue change:" + str(average_rev_change))
+
+    max_increase = max(rev_change) # max function is used to get the max value in the revenue change for the greatest increase in revenue in each month
+    max_decrease = min(rev_change) # min function is used to get the max value in the revenue change for the greatest decrease in revenue in each month
+    print("Greatest Increase in the revenue:" + str(max_increase))
+    print("Greatest Decrease in the revenue:" + str(max_decrease))
+
+
