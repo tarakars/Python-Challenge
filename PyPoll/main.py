98e2f70fd302c5d288f9f63fbceb8fead10016a8
Taraka Rajeev Sharma Narayanabatla <tarakars_n@hotmail.com>
2018-03-07 22:03:50 -0500

    Home Work

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,4 +1,4 @@
-#Import the libraries
+# First is first, Import the libraries
 import csv
 import os
 import pandas as pd
@@ -11,43 +11,48 @@ candidates=[] #declaring this array to store the candidates colomun
 final_candidates=[] #declaring this array to store the unique candiadate in the election
 candidates_count=[] # declaring this array to store the no.of votes pe candidate
 
-output = open("outputfile.txt","w")
+output = open("outputfile.txt","w") #created and opened a text file to write the results. 'w' denote the write function
 
 election_data_path = os.path.join("raw_data", "election_data_1.csv") # declaring the path for the raw data we have
 
+#printing the sentences in command prompt usinf print function and output.writelines is used to write the result in text file
 print("Election Results")
 output.writelines("Election Results")
 print("----------------------------")
 output.writelines("\n" + "----------------------------------------")
 
-with open(election_data_path, newline="") as csvfile:
-    election_data = csv.reader(csvfile, delimiter = ",")
+with open(election_data_path, newline="") as csvfile: #opening thr CSV file
+    election_data = csv.reader(csvfile, delimiter = ",") #reading the CSV file in to a list
 
-    next(election_data,None)
+    next(election_data,None) # skipping thr first row to make sure we dont count header in the analysis
 
+    #looping through the data we read in the list to do the calculations
     for row in election_data:
-        candidates.append(row[2])
-        votes.append(row[0])
-    total_votes = len(votes)
-    print("Total Votes:" + str(total_votes))
-    output.writelines("\n" + "Total Votes:" + str(total_votes))
+        candidates.append(row[2]) # appending the 3rd value of election_data to another list called candidates
+        votes.append(row[0]) # appending the 1st value in the eletiond_data list to a new list called votes
+    total_votes = len(votes) #using len function to get the lenght of the votes list, so that we get total no.of votes
+    print("Total Votes:" + str(total_votes)) # printing the total votes we stored
+    output.writelines("\n" + "Total Votes:" + str(total_votes)) # writing the total votes in a text file
     print("----------------------------------")
     output.writelines("\n" + "----------------------------------------")
 
+    #looping through the array(candidates), we created to get the unique candidates
     for i in candidates:
-        if i not in final_candidates:
-            final_candidates.append(i)
+        if i not in final_candidates: # condition to check if the candidate is already in the final candidate list
+            final_candidates.append(i) #if the candidate is not in the list appending them to the final candidates list
+
+    #loping through the range of 1 to the length of final candidates to get the no of votes per person
     for j in range(len(final_candidates)):
-            candidates_count.append(candidates.count(final_candidates[j]))
-            percentage.append((candidates_count[j]/total_votes)*100)
-            print(final_candidates[int(j)]+ " : " + str(candidates_count[int(j)]) + " ( " + str("{0:.2f}%".format(percentage[j])) + " ) ")
+            candidates_count.append(candidates.count(final_candidates[j])) # appending the votes per candidate in the candidates_count array by using count funcion
+            percentage.append((candidates_count[j]/total_votes)*100) # calculating the percentage of votes a candidate accquired
+            print(final_candidates[int(j)]+ " : " + str(candidates_count[int(j)]) + " ( " + str("{0:.2f}%".format(percentage[j])) + " ) ") # printing all the results for each candidate
             output.writelines("\n" + final_candidates[int(j)]+ " : " + str(candidates_count[int(j)]) + " ( " + str("{0:.2f}%".format(percentage[j])) + " ) ") 
 
-    winner = max(candidates)
+    winner = max(candidates) # getting the winner of the election by determining how many times the same canidate has been voted by using candidate array
     print("----------------------------------")
     output.writelines("\n" + "----------------------------------------")
-    print("Winner:" + winner)
-    output.writelines("\n" + "Winner:" + winner)
+    print("Winner:" + winner) # printing the winner
+    output.writelines("\n" + "Winner:" + winner) # writing the winner in text file
     print("----------------------------------")
     output.writelines("\n" + "----------------------------------------")
 
--- Pybank/main.py
+++ Pybank/main.py
@@ -1,16 +1,15 @@
-#Import Libraries 
+# first is first - Import Libraries 
 import csv
 import os
-import pandas as pd
-import numpy
+import sys
 
 revenue=[] # declare and array to store revenue as a list from the csv file
 date=[] # declare and array to store date as a list from the csv file
 rev_change=[] # declare and array to store change in revenue as list
 
 
-print("Financial Analysis")
-print("----------------------------------------")
+#print("Financial Analysis")
+#print("----------------------------------------")
 
 
 budget_data_path = os.path.join("raw_data","budget_data_1.csv") # declaring the path for the CSV
@@ -22,27 +21,47 @@ with open(budget_data_path,newline="") as csvfile: # making sure our operations 
     for row in budget_data: # creating a loop through the csv and create two arrays revenue and date
         revenue.append(float(row[1]))
         date.append(row[0])
-    print("Total number of months:" + str(len(date))) # len function is used to get the length of the array so we know how many months we have
-    print("Total Revenue:" + str(sum(revenue))) # sum function is used to get the sum of the array so we know the revenue
+    no_of_months = len(date)
+    sum_revernue = sum(revenue)
+    #print("Total number of months:" + str(no_of_months)) # len function is used to get the length of the array so we know how many months we have
+    #print("Total Revenue:" + str(sum_revernue)) # sum function is used to get the sum of the array so we know the revenue
 
     for i in range(1,len(revenue)): # creating a loop through the revenue array to calculate the average revenue change
         rev_change.append(revenue[i] - revenue[i-1])
     average_rev_change = sum(rev_change)/len(rev_change)
-    print("average revenue change:" + str(average_rev_change))
+    #print("average revenue change:" + str(average_rev_change))
     max_increase = max(rev_change) # max function is used to get the max value in the revenue change for the greatest increase in revenue in each month
     max_decrease = min(rev_change) # min function is used to get the max value in the revenue change for the greatest decrease in revenue in each month
-    print("Greatest Increase in the revenue:" + str(max_increase))
-    print("Greatest Decrease in the revenue:" + str(max_decrease))
+    #print("Greatest Increase in the revenue:" + str(max_increase))
+    #print("Greatest Decrease in the revenue:" + str(max_decrease))
+
+financial_analysis = (
+    f"\nFinancial Analysis"
+    f"\n----------------------------------------"
+    f"\nTotal number of months: {no_of_months}" # len function is used to get the length of the array so we know how many months we have
+    f"\nTotal Revenue: {sum_revernue}" # sum function is used to get the sum of the array so we know the revenue
+    f"\naverage revenue change: {average_rev_change}"
+    f"\nGreatest Increase in the revenue: {max_increase}"
+    f"\nGreatest Decrease in the revenue: {max_decrease}"
+
+)
+print(financial_analysis,end = "")
+
+sys.stdout = open("outputfile.txt", 'w')
+print(financial_analysis)
+
+
+
 
 #creating a text file and writing our outputs in the file
-output = open("outputfile.txt","w")
-output.writelines("Financial Analysis")
-output.writelines("\n" + "----------------------------------------")
-output.writelines("\n" + "Total number of months:" + str(len(date)))
-output.writelines("\n" + "Total Revenue:" + str(sum(revenue)))
-output.writelines("\n" + "average revenue change:" + str(average_rev_change))
-output.writelines("\n" + "Greatest Increase in the revenue:" + str(max_increase))
-output.writelines("\n" + "Greatest Decrease in the revenue:" + str(max_decrease))
+#output = open("outputfile.txt","w")
+#output.writelines("Financial Analysis")
+#output.writelines("\n" + "----------------------------------------")
+#output.writelines("\n" + "Total number of months:" + str(len(date)))
+#output.writelines("\n" + "Total Revenue:" + str(sum(revenue)))
+#output.writelines("\n" + "average revenue change:" + str(average_rev_change))
+#output.writelines("\n" + "Greatest Increase in the revenue:" + str(max_increase))
+#output.writelines("\n" + "Greatest Decrease in the revenue:" + str(max_decrease))
 
 
 
