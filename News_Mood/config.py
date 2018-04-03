c24761b35d4d10024d88d92fc1a2feffb3a79709
Taraka Rajeev Sharma Narayanabatla <tarakars_n@hotmail.com>
2018-04-03 18:05:56 -0400

    Home Work

---
+++ News_Mood/config.py
@@ -0,0 +1,4 @@
+consumer_key = "9UGLaZRCowes9zRTD1Lwrvbm8"
+consumer_secret = "wWxv9IrYFHmtW22VDs6oIWnh3U2wQTgePCWGNyDAn5X0VHBnFu"
+access_token = "977544509595750401-8uvHxCEUUaNRRTr4NAQjvGuMbTBglxP"
+access_token_secret = "QabHwWFFzHa8uYUkxUAZFrcYlcs3lUixFppF20NXwY6GL"
